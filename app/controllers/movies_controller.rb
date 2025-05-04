cdba31fcd3c02acb9fe9bd17049bd5089561be2b
evnora <enora0@chicagobooth.edu>
2025-05-04 20:34:37 +0000

    commit

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,24 @@
 class MoviesController < ApplicationController
+  def update
+    m_id = params.fetch("an_id")                           # matches your route
+    matching_records = Movie.where({ :id => m_id })
+    the_movie = matching_records.at(0)  
+    the_movie.title = params.fetch("the_title")
+    the_movie.year = params.fetch("the_year")
+    the_movie.duration = params.fetch("the_duration")
+    the_movie.description = params.fetch("the_description")
+    the_movie.image = params.fetch("the_image")
+    the_movie.director_id = params.fetch("the_director_id")
+
+    # Save 
+
+    the_movie.save
+
+    # Redirect to the movie details page 
+
+    redirect_to("/movies/#{the_movie.id}")
+  end 
+  
   def create 
 
     m = Movie.new
