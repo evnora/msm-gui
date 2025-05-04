ff7a7834f70887881170a3488cb3158f991800a8
evnora <enora0@chicagobooth.edu>
2025-05-04 20:47:29 +0000

    commit

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,17 +1,49 @@
 class ActorsController < ApplicationController
   def index
     matching_actors = Actor.all
-    @list_of_actors = matching_actors.order({ :created_at => :desc })
+    @list_of_actors = matching_actors
 
-    render({ :template => "actor_templates/index" })
+    render({ :template => "actors/index" })
   end
 
   def show
     the_id = params.fetch("path_id")
-
     matching_actors = Actor.where({ :id => the_id })
-    @the_actor = matching_actors.at(0)
-      
-    render({ :template => "actor_templates/show" })
+    @actor = matching_actors.at(0)
+
+    render({ :template => "actors/show" })
+  end
+
+  def create
+    actor = Actor.new
+    actor.name  = params.fetch("query_name")
+    # if you have an image field on your create form uncomment:
+    # actor.image = params.fetch("query_image", "")
+    actor.save
+
+    redirect_to("/actors")
+  end
+
+  def update
+    the_id = params.fetch("an_id")
+    matching_actors = Actor.where({ :id => the_id })
+    actor = matching_actors.at(0)
+
+    actor.image = params.fetch("query_image")
+    # if you also allow changing the name:
+    # actor.name = params.fetch("query_name")
+    actor.save
+
+    redirect_to("/actors/#{the_id}")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_actors = Actor.where({ :id => the_id })
+    actor = matching_actors.at(0)
+
+    actor.destroy
+
+    redirect_to("/actors")
   end
 end
--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,39 +1,70 @@
 class DirectorsController < ApplicationController
   def index
     matching_directors = Director.all
-    @list_of_directors = matching_directors.order({ :created_at => :desc })
+    @list_of_directors = matching_directors
 
-    render({ :template => "director_templates/index" })
+    render({ :template => "directors/index" })
   end
 
   def show
     the_id = params.fetch("path_id")
-
     matching_directors = Director.where({ :id => the_id })
-    @the_director = matching_directors.at(0)
+    @director = matching_directors.at(0)
 
-    render({ :template => "director_templates/show" })
+    # if you want to list their movies:
+    # @filmography = Movie.where({ :director_id => @director.id })
+
+    render({ :template => "directors/show" })
+  end
+
+  def create
+    director = Director.new
+    director.name = params.fetch("query_name")
+    # if you have other fields on your create form (dob, image, bio):
+    # director.dob   = params.fetch("query_dob")
+    # director.image = params.fetch("query_image", "")
+    # director.bio   = params.fetch("query_bio", "")
+    director.save
+
+    redirect_to("/directors")
+  end
+
+  def update
+    the_id = params.fetch("an_id")
+    matching_directors = Director.where({ :id => the_id })
+    director = matching_directors.at(0)
+
+    director.image = params.fetch("query_image")
+    # you can also update name, bio, etc. if your form includes them
+    # director.name = params.fetch("query_name")
+    director.save
+
+    redirect_to("/directors/#{the_id}")
+  end
+
+  def destroy
+    the_id = params.fetch("an_id")
+    matching_directors = Director.where({ :id => the_id })
+    director = matching_directors.at(0)
+
+    director.destroy
+
+    redirect_to("/directors")
   end
 
   def max_dob
-    directors_by_dob_desc = Director.
-      all.
-      where.not({ :dob => nil }).
-      order({ :dob => :desc })
+    # youngest director (largest dob)
+    youngest_collection = Director.where({}).order({ :dob => :desc })
+    @director = youngest_collection.at(0)
 
-    @youngest = directors_by_dob_desc.at(0)
-
-    render({ :template => "director_templates/youngest" })
+    render({ :template => "directors/max_dob" })
   end
 
   def min_dob
-    directors_by_dob_asc = Director.
-      all.
-      where.not({ :dob => nil }).
-      order({ :dob => :asc })
-      
-    @eldest = directors_by_dob_asc.at(0)
+    # eldest director (smallest dob)
+    eldest_collection = Director.where({}).order({ :dob => :asc })
+    @director = eldest_collection.at(0)
 
-    render({ :template => "director_templates/eldest" })
+    render({ :template => "directors/min_dob" })
   end
 end
