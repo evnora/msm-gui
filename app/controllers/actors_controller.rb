9fd08a2ecce97904a7d65b69ed11be49be72d19b
evnora <enora0@chicagobooth.edu>
2025-05-04 22:42:29 +0000

    commit: actors and directors working

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,49 +1,40 @@
 class ActorsController < ApplicationController
   def index
-    matching_actors = Actor.all
-    @list_of_actors = matching_actors
-
+    @list_of_actors = Actor.all
     render({ :template => "actors/index" })
   end
 
   def show
-    the_id = params.fetch("path_id")
-    matching_actors = Actor.where({ :id => the_id })
+    matching_actors = Actor.where({ :id => params.fetch("path_id") })
     @actor = matching_actors.at(0)
-
     render({ :template => "actors/show" })
   end
 
   def create
     actor = Actor.new
-    actor.name  = params.fetch("query_name")
-    # if you have an image field on your create form uncomment:
-    # actor.image = params.fetch("query_image", "")
+    actor.name = params.fetch("the_name")
+    actor.dob  = params.fetch("the_dob")
+    actor.bio  = params.fetch("the_bio")
+    actor.image = params.fetch("the_image")
     actor.save
+    redirect_to("/actors")
+  end
 
+  def destroy
+    matching_actors = Actor.where({ :id => params.fetch("an_id") })
+    actor = matching_actors.at(0)
+    actor.destroy
     redirect_to("/actors")
   end
 
   def update
-    the_id = params.fetch("an_id")
-    matching_actors = Actor.where({ :id => the_id })
+    matching_actors = Actor.where({ :id => params.fetch("an_id") })
     actor = matching_actors.at(0)
-
-    actor.image = params.fetch("query_image")
-    # if you also allow changing the name:
-    # actor.name = params.fetch("query_name")
+    actor.name  = params.fetch("the_name")
+    actor.dob   = params.fetch("the_dob")
+    actor.bio   = params.fetch("the_bio")
+    actor.image = params.fetch("the_image")
     actor.save
-
-    redirect_to("/actors/#{the_id}")
-  end
-
-  def destroy
-    the_id = params.fetch("an_id")
-    matching_actors = Actor.where({ :id => the_id })
-    actor = matching_actors.at(0)
-
-    actor.destroy
-
-    redirect_to("/actors")
+    redirect_to("/actors/#{ actor.id }")
   end
 end
--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,70 +1,54 @@
 class DirectorsController < ApplicationController
   def index
-    matching_directors = Director.all
-    @list_of_directors = matching_directors
-
+    @list_of_directors = Director.all
     render({ :template => "directors/index" })
   end
 
   def show
-    the_id = params.fetch("path_id")
-    matching_directors = Director.where({ :id => the_id })
+    matching_directors = Director.where({ :id => params.fetch("path_id") })
     @director = matching_directors.at(0)
-
-    # if you want to list their movies:
-    # @filmography = Movie.where({ :director_id => @director.id })
-
     render({ :template => "directors/show" })
   end
 
   def create
     director = Director.new
-    director.name = params.fetch("query_name")
-    # if you have other fields on your create form (dob, image, bio):
-    # director.dob   = params.fetch("query_dob")
-    # director.image = params.fetch("query_image", "")
-    # director.bio   = params.fetch("query_bio", "")
+    director.name  = params.fetch("the_name")
+    director.dob   = params.fetch("the_dob")
+    director.bio   = params.fetch("the_bio")
+    director.image = params.fetch("the_image")
     director.save
+    redirect_to("/directors")
+  end
 
+  def destroy
+    matching_directors = Director.where({ :id => params.fetch("an_id") })
+    director = matching_directors.at(0)
+    director.destroy
     redirect_to("/directors")
   end
 
   def update
-    the_id = params.fetch("an_id")
-    matching_directors = Director.where({ :id => the_id })
+    matching_directors = Director.where({ :id => params.fetch("an_id") })
     director = matching_directors.at(0)
-
-    director.image = params.fetch("query_image")
-    # you can also update name, bio, etc. if your form includes them
-    # director.name = params.fetch("query_name")
+    director.name  = params.fetch("the_name")
+    director.dob   = params.fetch("the_dob")
+    director.bio   = params.fetch("the_bio")
+    director.image = params.fetch("the_image")
     director.save
-
-    redirect_to("/directors/#{the_id}")
-  end
-
-  def destroy
-    the_id = params.fetch("an_id")
-    matching_directors = Director.where({ :id => the_id })
-    director = matching_directors.at(0)
-
-    director.destroy
-
-    redirect_to("/directors")
+    redirect_to("/directors/#{ director.id }")
   end
 
   def max_dob
-    # youngest director (largest dob)
-    youngest_collection = Director.where({}).order({ :dob => :desc })
-    @director = youngest_collection.at(0)
-
+    # youngest = largest dob
+    ordered = Director.order({ :dob => :desc })
+    @director = ordered.at(0)
     render({ :template => "directors/max_dob" })
   end
 
   def min_dob
-    # eldest director (smallest dob)
-    eldest_collection = Director.where({}).order({ :dob => :asc })
-    @director = eldest_collection.at(0)
-
+    # eldest = smallest dob
+    ordered = Director.order({ :dob => :asc })
+    @director = ordered.at(0)
     render({ :template => "directors/min_dob" })
   end
 end
